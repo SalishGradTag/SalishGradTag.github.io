d3879803b40f2b964683d7350fe92242e6a87032
Yash Jain <jainy7002@gmail.com>
2025-02-23 16:17:11 -0800

    Added harangad missed photo

--- src/messagejson.py
+++ src/messagejson.py
@@ -11,19 +11,21 @@ with open(json_file) as json_data:
     players = data["players"]
 
     for name in names:
-        cur = {}
-        cur["email"] = players[name]["Email"]
-        cur["message"] = f"""
-Hi this is testing for gradtag. THIS IS ONLY A TEST. <br>
+        if (name=="Harnagad Sidhu"):
+            cur = {}
+            cur["email"] = players[name]["Email"]
+            cur["message"] = f"""
+Sorry Harangad still testing <br>
 
-Hi {name.split(' ')[0]}, <br>
+Hi <at>{cur["email"]}</at>, <br>
 Your target is {players[name]["Target"]}. <br>
 
 Please read the <a href="https://salishgradtag.ca/2025/rules">rules</a> <br>
+
 If you have questions or want to submit a tag, message myself, Logan Singh or @salishgradtag on instagram.
 """
         
-        messages.append(cur)
+            messages.append(cur)
         
 with open('src/messages.json', 'w', encoding='utf-8') as f:
     json.dump(messages, f, ensure_ascii=False, indent=4)
