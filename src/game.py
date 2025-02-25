1223bdda8b619257588800ed84252004dff4ebb4
Yash Jain <jainy7002@gmail.com>
2025-02-24 18:24:41 -0800

    Added 2 people

--- src/game.py
+++ src/game.py
@@ -29,6 +29,8 @@ for index, row in player_data.iterrows():
         "Email" : row["Email"],
         "HandleIG" : row["HandleIG"],
         "Photo" : getPath(row["Photo"]),
+        "Tagger" : "",
+        "Date" : "",
         "Ranking" : -1,
         "Alive" : True,
         "Tags" : 0,
@@ -42,8 +44,11 @@ for index, row in game_log.iterrows():
     command = row["Command"]
     p1 = row["Para1"]
     p2 = row["Para2"]
+    date = row["Date"]
     time = row["Unix"]
-
+    if (command=="Point"):
+        players[p1]["Tags"]+=1
+        players[p1]["Last Tagged"] = time
     if (command=="Shuffle"):
         names.sort(key=lambda x:players[x]["Alive"], reverse=True)
         a = list(range(0,num))
@@ -55,9 +60,12 @@ for index, row in game_log.iterrows():
     if (row["Command"]=="Tag"):
         players[p1]["Tags"]+=1
         players[p1]["Last Tagged"] = time
+        players[p1]["Date"] = str(date)[5:10]
         players[p2]["Alive"] = False
         players[p2]["Last Tagged"] = time
-        
+        players[p2]["Date"] = str(date)[5:10]
+
+        players[p2]["Tagger"] = p1
         players[p1]["Target"] = players[p2]["Target"]
         num-=1
      
@@ -86,6 +94,10 @@ for name in names:
         players[name]["HTML"] += '<img style="width:100px;" src="../photos/' + players[name]["Photo"] + '"><br> Rank: ' + str(players[name]["Rank"]) + '<br>' + players[name]["Name"]
     else:
         players[name]["HTML"] += '<img style="width:100px;" src="../Images/TaggedStamp.png' + '"><br>Rank: ' + str(players[name]["Rank"]) + '<br>' + players[name]["Name"]
+        players[name]["HTML"] += f'<br>Tagged on {players[name]["Tagger"]} on {players[name]["Date"]}'
+    
+    if (players[name]["Tags"]>0):
+        players[name]["HTML"] += f'<br>{players[name]["Tags"]} tags'
         
         
 json_data = {
